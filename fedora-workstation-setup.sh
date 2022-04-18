69fd94c7281e9cbd9e29c4b7e72afe0843e3e103
Cliff Mosley <cliff@mosley.me>
2022-04-18 13:25:54 -0700

    nvim installation steps, moved some apps to flatpak

--- fedora-workstation-setup.sh
+++ fedora-workstation-setup.sh
@@ -3,7 +3,6 @@ echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
 echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
 cat /etc/dnf/dnf.conf
 
-
 sudo dnf upgrade --refresh
 sudo dnf check
 sudo dnf autoremove
@@ -15,43 +14,74 @@ sudo reboot now
 sudo dnf groupupdate core
 
 sudo dnf install -y rpmfusion-free-release-tainted
-sudo dnf install -y rpmfusion-nonfree-release-tainted 
+sudo dnf install -y rpmfusion-nonfree-release-tainted
 sudo dnf install -y dnf-plugins-core
-sudo dnf install -y *-firmware 
+sudo dnf install -y *-firmware
 sudo dnf -y groupupdate sound-and-video
 
+# enable the other repos
 sudo dnf install -y fedora-workstation-repositories
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y gnome-tweak-tool
 
+# install chrome
 sudo dnf config-manager --set-enabled google-chrome
 sudo dnf install -y google-chrome-stable
 
-sudo rpm --import https://packages.microsoft.com/keys/microsoftfish.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update
-sudo dnf install -y code
-
+# install some of the core things from repos
+sudo dnf install -y git
 sudo dnf install -y graphviz
 sudo dnf install -y gimp
-sudo dnf install -y vlc
-sudo dnf install -y qbittorrent 
 sudo dnf install -y fira-code-fonts 'mozilla-fira*' 'google-roboto*'
 
+# set up fish shell and enable vim keybinds
 sudo dnf install -y fish util-linux-user
 sudo dnf install -y curl
 chsh -s $(which fish)
 curl -L https://get.oh-my.fish | fish
 mkdir -p /home/$USER/.local/bin
 set -Ua fish_user_paths /home/$USER/.local/bin
+set -Ua fish_vi_key_bindings
 
+# power profiles - remove legacy and install new
+sudo dnf remove power-profiles-daemon
 sudo dnf install -y tlp tlp-rdw
 sudo systemctl enable tlp
 
-sudo dnf copr enable kwizart/fedy
-sudo dnf install fedy -y
+# configure the user interface and swap escape to cap lock
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.5
 
-# slack is manual download from slack site or via fedy
-# zoom is manual download or via fedy
+# install all the random apps that I just need
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpack install -y flathub com.visualstudio.code
+sudo flatpack install -y flathub md.obsidian.Obsidian
+sudo flatpack install -y flathub us.zoom.Zoom
+sudo flatpack install -y flathub com.slack.Slack
+sudo flatpack install -y flathub org.videolan.VLC
+sudo flatpack install -y flathub org.qbittorrent.qBittorrent
 
+# install nvim and get it configured
+sudo dnf copr enable agriffis/neovim-nightly
+sudo dnf install -y neovim python3-neovim
+
+# c++ prerequisites for nvim
+sudo dnf install -y gcc-c++
+
+# pip prerequisites for nvim
+sudo dnf install -y python3-pip
+sudo pip3 install pynvim
+
+# install the npm which will be required for
+# vim language servers
+sudo dnf install -y npm
+sudo npm install -g bash-language-server pyright vscode-langservers-extracted
+
+mkdir /home/$USER/git
+cd /home/$USER/git
+git clone https://github.com/brainfucksec/neovim-lua.git
+cd neovim-lua/
+cp -Rv nvim ~/.config/
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim +PackerSync
